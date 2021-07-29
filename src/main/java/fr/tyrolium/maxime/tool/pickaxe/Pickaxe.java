6fcbe3e2989cbe822580a221e5198dfe0765c4c8
TheMaximeSan <mxm.tournier@gmail.com>
2021-07-30 01:26:05 +0200

    add v1

--- src/main/java/fr/tyrolium/maxime/tool/pickaxe/Pickaxe.java
+++
@@ -1,9 +0,0 @@
-package fr.tyrolium.maxime.tool.pickaxe;
-
-import net.minecraft.item.ItemPickaxe;
-
-public class Pickaxe extends ItemPickaxe {
-    protected Pickaxe(ToolMaterial p_i45347_1_) {
-        super(p_i45347_1_);
-    }
-}
