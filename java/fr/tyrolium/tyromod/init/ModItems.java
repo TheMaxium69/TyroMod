b9859f81e1f6abdc148b46562e2591591ed78c9f
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2022-07-12 23:15:58 +0200

    sword

--- java/fr/tyrolium/tyromod/init/ModItems.java
+++ java/fr/tyrolium/tyromod/init/ModItems.java
@@ -1,8 +1,10 @@
 package fr.tyrolium.tyromod.init;
 
 import fr.tyrolium.tyromod.TyroMod;
+import fr.tyrolium.tyromod.utils.CustomItemTiers;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,7 +14,9 @@ public class ModItems {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TyroMod.ModId);
 
-    public static final RegistryObject<Item> TYROLIUM_SWORD = ITEMS.register("tyrolium_sword", () -> new Item(new Item.Properties().stacksTo(1).tab(ItemGroup.TAB_COMBAT)));
     public static final RegistryObject<Item> TYROLIUM = ITEMS.register("tyrolium", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
+
+    public static final RegistryObject<Item> TYROLIUM_SWORD = ITEMS.register("tyrolium_sword", () -> new SwordItem(CustomItemTiers.TYROLIUM, 2, -2.4f, new Item.Properties().stacksTo(1).tab(ItemGroup.TAB_TOOLS)));
+
 
 }
