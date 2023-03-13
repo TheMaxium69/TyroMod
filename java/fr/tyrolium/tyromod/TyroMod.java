f2e3d60b2317e2f23b28280f8308a08d88ffbb07
Maxime Tournier <mxm.tournier@gmail.com>
2023-03-13 02:45:20 +0100

    update all

--- java/fr/tyrolium/tyromod/TyroMod.java
+++ java/fr/tyrolium/tyromod/TyroMod.java
@@ -2,27 +2,39 @@ package fr.tyrolium.tyromod;
 
 import fr.tyrolium.tyromod.init.ModBlocks;
 import fr.tyrolium.tyromod.init.ModItems;
+import fr.tyrolium.tyromod.network.PacketToken;
+import fr.tyrolium.tyromod.security.LauncherToken;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.NetworkDirection;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
+import net.minecraftforge.fml.server.ServerLifecycleHooks;
+import org.apache.logging.log4j.LogManager;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Scanner;
+
+import java.util.Optional;
 
 @Mod(TyroMod.ModId)
 public class TyroMod {
 
     public static final String ModId = "tyromod";
 
+    public static final String PROTOCOL_VERSION = "1";
+    public static final SimpleChannel NETWORK = NetworkRegistry.newSimpleChannel(new ResourceLocation(ModId, "channel"), () -> PROTOCOL_VERSION, PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals);
+
 
     public TyroMod() {
+
+        LauncherToken.Token();
+        System.out.println("GetToken : " + LauncherToken.getTokenUser() + " GetTokenOld : " + LauncherToken.getTokenUserOld());
 
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
@@ -31,17 +43,37 @@ public class TyroMod {
         ModItems.ITEMS.register(bus);
         ModBlocks.BLOCKS.register(bus);
 
-
-
     }
 
     private void setup(FMLCommonSetupEvent e){
+
+        int index = 0;
+        NETWORK.registerMessage(index, PacketToken.class, PacketToken::encode, PacketToken::decode, PacketToken::handle, Optional.of(NetworkDirection.PLAY_TO_SERVER));
 
     }
 
     private void clientSetup(FMLClientSetupEvent e){
 
     }
+
+    /*@Mod.EventBusSubscriber
+    public static class EventBus {
+
+        @SubscribeEvent
+        public static void onEvent(EntityJoinWorldEvent event) {
+            if ((event.getEntity() instanceof PlayerEntity)) {
+
+                System.out.println("Connecte");
+
+                NETWORK.sendToServer(new PacketToken(LauncherToken.getTokenUser()));
+
+            }
+        }
+
+    }*/
+
+
+
 
 
 }
--- java/fr/tyrolium/tyromod/network/PacketToken.java
+++ java/fr/tyrolium/tyromod/network/PacketToken.java
@@ -1,2 +1,37 @@
-package fr.tyrolium.tyromod.network;public class PacketToken {
+package fr.tyrolium.tyromod.network;
+
+import fr.tyrolium.tyromod.security.LauncherToken;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.fml.network.NetworkEvent;
+
+import java.util.function.Supplier;
+
+public class PacketToken {
+
+    public String Token;
+
+    public PacketToken(String token) {
+        this.Token = token;
+    }
+
+    public static void encode(PacketToken packetToken, PacketBuffer packetBuffer) {
+        packetBuffer.writeUtf(packetToken.Token);
+    }
+
+    public static PacketToken decode(PacketBuffer packetBuffer) {
+        String Token = packetBuffer.readUtf();
+        return new PacketToken(Token);
+    }
+
+    public static void handle(PacketToken packet, Supplier<NetworkEvent.Context> contextSupplier) {
+
+        ServerPlayerEntity player = contextSupplier.get().getSender();
+
+        String Token = packet.Token;
+
+        player.displayClientMessage(new StringTextComponent(Token), true);
+
+    }
 }
