44b011cf4caecd843e0e94eb6130a91dc3d82b31
Maxime Tournier <mxm.tournier@gmail.com>
2024-05-01 15:38:55 +0200

    bugfix

--- java/fr/tyrolium/tyromod/init/BlocksMod.java
+++ java/fr/tyrolium/tyromod/init/BlocksMod.java
@@ -22,13 +22,10 @@ public class BlocksMod {
     public static final List<Block> BLOCKS = new ArrayList<Block>();
 
     /* MANUEL */
-    public static Block ELEVATOR_BLOCK = new ElevatorBlock("elevator_block", Material.ANVIL, "3", "cb999999");
+    public static Block ELEVATOR_BLOCK = new ElevatorBlock("elevetor_block", Material.ANVIL, "3", "cb999999");
 
     /* GENERATION */
     public static BlockGenerateClass[] BlockList = {
-
-
-            new BlockGenerateClass("tyrolium", "ore", true, "1", "IUM"),
 
             /*TYROLIUM*/
             new BlockGenerateClass("tyrolium", "ore", true, "1", "IUM"),
