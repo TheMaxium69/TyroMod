555c27b37e0c595af655b0f27c98985a9a5b354e
Maxime Tournier <mxm.tournier@gmail.com>
2024-05-01 17:02:46 +0200

    radar is checked

--- java/fr/tyrolium/tyromod/items/Radar.java
+++ java/fr/tyrolium/tyromod/items/Radar.java
@@ -1,10 +1,69 @@
 package fr.tyrolium.tyromod.items;
 
 import fr.tyrolium.tyromod.global.DefaultItem;
+import fr.tyrolium.tyromod.tileentity.gui.GuiRadar;
+import fr.tyrolium.tyromod.util.handlers.IHasModel;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.IItemPropertyGetter;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class Radar extends DefaultItem {
+public class Radar extends DefaultItem  implements IHasModel {
+
+    private static EntityPlayer player;
+
+    private static int dura = 90 * 20 * 60;
+
+    public static int percents = 0;
+
     public Radar(String name, String version, String id)
     {
         super(name, version, id);
+        setMaxStackSize(1);
+        this.setMaxDamage(this.dura);
+        this.addPropertyOverride(new ResourceLocation("percent"), new IItemPropertyGetter()
+        {
+
+            @SideOnly(Side.CLIENT)
+            @Override
+            public float apply(ItemStack stack, World worldIn, EntityLivingBase entityIn)
+            {
+                return percents;
+            }
+        });
+
     }
+
+
+    @Override
+    public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected)
+    {
+        this.player = (EntityPlayer)entityIn;
+
+        if(this.player.getHeldItem(EnumHand.MAIN_HAND).getItem() instanceof Radar)
+        {
+            if(this.dura >= 0)
+            {
+                this.dura--;
+                stack.damageItem(1, player);
+                if(GuiRadar.amountTiles <= 25)
+                {
+                    this.percents = GuiRadar.amountTiles;
+                }
+                else if(GuiRadar.amountTiles >= 26)
+                {
+                    this.percents = 26;
+                }
+            }
+        }
+
+        super.onUpdate(stack, worldIn, entityIn, itemSlot, isSelected);
+    }
+
 }
--- java/fr/tyrolium/tyromod/proxy/Client.java
+++ java/fr/tyrolium/tyromod/proxy/Client.java
@@ -1,8 +1,10 @@
 package fr.tyrolium.tyromod.proxy;
 
+import fr.tyrolium.tyromod.tileentity.gui.GuiRadar;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.MinecraftForge;
 
 public class Client extends CommonProxy{
 
@@ -10,6 +12,7 @@ public class Client extends CommonProxy{
     public void registerModel(Item item, int metadata)
     {
         ModelLoader.setCustomModelResourceLocation(item, metadata, new ModelResourceLocation(item.getRegistryName(), "inventory"));
+        MinecraftForge.EVENT_BUS.register(new GuiRadar());
     }
 
 }
