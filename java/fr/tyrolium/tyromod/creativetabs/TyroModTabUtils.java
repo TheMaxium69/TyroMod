9b1136f29c21f0b36b263c2637f772e55e570922
Maxime Tournier <mxm.tournier@gmail.com>
2024-05-01 16:29:30 +0200

    Update TyroModTabUtils.java

--- java/fr/tyrolium/tyromod/creativetabs/TyroModTabUtils.java
+++ java/fr/tyrolium/tyromod/creativetabs/TyroModTabUtils.java
@@ -14,6 +14,6 @@ public class TyroModTabUtils extends CreativeTabs {
 
     @Override
     public ItemStack getTabIconItem() {
-        return new ItemStack(BlocksMod.getBlockCustomClass("bc33"));
+        return new ItemStack(BlocksMod.getBlockCustomClass("bc32"));
     }
 }
