3959183039943eb7d4cf07d55b312e11e531b280
TheMaxium69 <mxm.tournier@gmail.com>
2024-06-03 00:45:59 +0200

    gen final

--- java/fr/tyrolium/tyromod/world/generation/WorldGenOres.java
+++ java/fr/tyrolium/tyromod/world/generation/WorldGenOres.java
@@ -13,47 +13,43 @@ import net.minecraftforge.fml.common.IWorldGenerator;
 
 import java.util.Random;
 
-public class WorldGenOres implements IWorldGenerator {
+public class WorldGenOres implements IWorldGenerator
+{
+    private WorldGenerator tin_ore;
 
-    private WorldGenerator ore_test;
-
-    public WorldGenOres(){
-
-        ore_test = new WorldGenMinable(BlocksMod.ELEVATOR_BLOCK.getDefaultState(), 6, BlockMatcher.forBlock(Blocks.STONE));
-
+    public WorldGenOres()
+    {
+        tin_ore = new WorldGenMinable(BlocksMod.BLOCKS.get(26).getDefaultState(), 6, BlockMatcher.forBlock(Blocks.STONE));
     }
 
     @Override
-    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
-
-        switch (world.provider.getDimension())
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider)
+    {
+        switch(world.provider.getDimension())
         {
             case -1:
                 break;
             case 0:
-                runGenerator(ore_test, world, random, chunkX, chunkZ, 150, 2, 60);
+                runGenerator(tin_ore, world, random, chunkX, chunkZ, 150, 2, 150);
                 break;
             case 1:
                 break;
-
         }
     }
 
-    private void runGenerator(WorldGenerator gen, World world, Random rand, int chunkX, int chunkZ, int chance, int minHeight, int maxHeight){
-
-        if (minHeight > maxHeight || minHeight < 0 || maxHeight > 256) throw new IllegalArgumentException("Ore generated out of bounds");
+    private void runGenerator(WorldGenerator gen, World world, Random rand, int chunkX, int chunkZ, int chance, int minHeight, int maxHeight)
+    {
+        if(minHeight > maxHeight || minHeight < 0 || maxHeight > 256) throw new IllegalArgumentException("Ore generated out of bounds");
 
         int heightDiff = maxHeight - minHeight + 1;
 
-        for (int i = 0; i < chance; i++) {
-
+        for(int i = 0; i < chance; i++)
+        {
             int x = chunkX * 16 + rand.nextInt(16);
-            int y = minHeight * rand.nextInt(heightDiff);
-            int z = chunkZ * 16 + rand.nextInt(heightDiff);
+            int y = minHeight + rand.nextInt(heightDiff);
+            int z = chunkZ * 16 + rand.nextInt(16);
 
-            gen.generate(world, rand, new BlockPos(x, y , z));
-
+            gen.generate(world, rand, new BlockPos(x, y, z));
         }
-
     }
 }
