0024452a6d92026b15f6025d6879411602347ea3
TheMaxium69 <mxm.tournier@gmail.com>
2024-06-27 15:09:50 +0200

    Update WorldGenOres.java

--- java/fr/tyrolium/tyromod/world/generation/WorldGenOres.java
+++ java/fr/tyrolium/tyromod/world/generation/WorldGenOres.java
@@ -28,16 +28,16 @@ public class WorldGenOres implements IWorldGenerator
 
     public WorldGenOres()
     {
-        tin_ore = new WorldGenMinable(BlocksMod.blocks[26].getDefaultState(), 6, BlockMatcher.forBlock(Blocks.STONE));
-        copper_ore = new WorldGenMinable(BlocksMod.blocks[10].getDefaultState(), 6, BlockMatcher.forBlock(Blocks.STONE));
-        mercure_ore = new WorldGenMinable(BlocksMod.blocks[17].getDefaultState(), 6, BlockMatcher.forBlock(Blocks.STONE));
-        enderine_ore = new WorldGenMinable(BlocksMod.blocks[13].getDefaultState(), 6, BlockMatcher.forBlock(Blocks.STONE));
-        sapphire_ore = new WorldGenMinable(BlocksMod.blocks[24].getDefaultState(), 6, BlockMatcher.forBlock(Blocks.STONE));
-        ruby_ore = new WorldGenMinable(BlocksMod.blocks[22].getDefaultState(), 6, BlockMatcher.forBlock(Blocks.STONE));
-        yellorite_ore = new WorldGenMinable(BlocksMod.blocks[7].getDefaultState(), 6, BlockMatcher.forBlock(Blocks.STONE));
-        volcanite_ore = new WorldGenMinable(BlocksMod.blocks[56].getDefaultState(), 6, BlockMatcher.forBlock(Blocks.STONE));
-        ium_ore = new WorldGenMinable(BlocksMod.blocks[100].getDefaultState(), 6, BlockMatcher.forBlock(Blocks.STONE));
-        netherite_ore = new WorldGenMinable(BlocksMod.getBlockCustomClass("bc54").getDefaultState(), 6, BlockMatcher.forBlock(Blocks.NETHERRACK));
+        tin_ore = new WorldGenMinable(BlocksMod.blocks[26].getDefaultState(), 6, BlockMatcher.forBlock(Blocks.STONE)); /*OUI*/
+        copper_ore = new WorldGenMinable(BlocksMod.blocks[10].getDefaultState(), 4, BlockMatcher.forBlock(Blocks.STONE)); /*OUI*/
+        mercure_ore = new WorldGenMinable(BlocksMod.blocks[17].getDefaultState(), 3, BlockMatcher.forBlock(Blocks.STONE)); /*OUI*/
+        enderine_ore = new WorldGenMinable(BlocksMod.blocks[13].getDefaultState(), 3, BlockMatcher.forBlock(Blocks.STONE));
+        sapphire_ore = new WorldGenMinable(BlocksMod.blocks[24].getDefaultState(), 3, BlockMatcher.forBlock(Blocks.STONE));
+        ruby_ore = new WorldGenMinable(BlocksMod.blocks[22].getDefaultState(), 3, BlockMatcher.forBlock(Blocks.STONE));
+        yellorite_ore = new WorldGenMinable(BlocksMod.blocks[7].getDefaultState(), 3, BlockMatcher.forBlock(Blocks.STONE));
+        volcanite_ore = new WorldGenMinable(BlocksMod.blocks[56].getDefaultState(), 3, BlockMatcher.forBlock(Blocks.STONE));
+        ium_ore = new WorldGenMinable(BlocksMod.blocks[100].getDefaultState(), 3, BlockMatcher.forBlock(Blocks.STONE));
+        netherite_ore = new WorldGenMinable(BlocksMod.getBlockCustomClass("bc54").getDefaultState(), 3, BlockMatcher.forBlock(Blocks.NETHERRACK));
     }
 
     @Override
@@ -46,18 +46,18 @@ public class WorldGenOres implements IWorldGenerator
         switch(world.provider.getDimension())
         {
             case -1:
-                runGenerator(netherite_ore, world, random, chunkX, chunkZ, 150, 2, 150);
+                runGenerator(netherite_ore, world, random, chunkX, chunkZ, 1, 13, 119);
                 break;
             case 0:
-                runGenerator(tin_ore, world, random, chunkX, chunkZ, 150, 2, 150);
-                runGenerator(copper_ore, world, random, chunkX, chunkZ, 150, 2, 150);
-                runGenerator(mercure_ore, world, random, chunkX, chunkZ, 150, 2, 150);
-                runGenerator(enderine_ore, world, random, chunkX, chunkZ, 150, 2, 150);
-                runGenerator(sapphire_ore, world, random, chunkX, chunkZ, 150, 2, 150);
-                runGenerator(ruby_ore, world, random, chunkX, chunkZ, 150, 2, 150);
-                runGenerator(yellorite_ore, world, random, chunkX, chunkZ, 150, 2, 150);
-                runGenerator(volcanite_ore, world, random, chunkX, chunkZ, 150, 2, 150);
-                runGenerator(ium_ore, world, random, chunkX, chunkZ, 150, 2, 150);
+                runGenerator(tin_ore, world, random, chunkX, chunkZ, 20, 18, 53);
+                runGenerator(copper_ore, world, random, chunkX, chunkZ, 10, 14, 40);
+                runGenerator(mercure_ore, world, random, chunkX, chunkZ, 6, 5, 30);
+                runGenerator(enderine_ore, world, random, chunkX, chunkZ, 1, 5, 12);
+                runGenerator(sapphire_ore, world, random, chunkX, chunkZ, 1, 5, 29);
+                runGenerator(ruby_ore, world, random, chunkX, chunkZ, 1, 5, 29);
+                runGenerator(yellorite_ore, world, random, chunkX, chunkZ, 1, 5, 11);
+                runGenerator(volcanite_ore, world, random, chunkX, chunkZ, 1, 5, 12);
+                runGenerator(ium_ore, world, random, chunkX, chunkZ, 1, 5, 15);
                 break;
             case 1:
                 break;
