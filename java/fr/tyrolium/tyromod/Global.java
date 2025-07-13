3dee0be9cf593415ab11e5c3f1d31ee5ebc6e062
TheMaxium69 <mxm.tournier@gmail.com>
2025-07-13 13:57:58 +0200

    Update Global.java

--- java/fr/tyrolium/tyromod/Global.java
+++ java/fr/tyrolium/tyromod/Global.java
@@ -13,8 +13,8 @@ public class Global {
     public static final String CLIENT_PROXY = "fr.tyrolium.tyromod.proxy.Client";
     public static final String SERVER_PROXY = "fr.tyrolium.tyromod.proxy.CommonProxy";
 
-//    public static final String API_FUSION = "http://localhost/Api-TyroModFusion/";
-    public static final String API_FUSION = "http://vps214.tyrolium.fr/api-fusion/";
+    public static final String API_FUSION = "http://localhost/Api-TyroModFusion/";
+//    public static final String API_FUSION = "http://vps214.tyrolium.fr/api-fusion/";
 
     public static final String API_USERITIUM = "http://useritium.fr/api-externe/index.php?controller=TyroServ&task=servVerif";
 
